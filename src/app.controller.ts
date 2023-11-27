17de53eab79e7a15cbb7701062d76fd662c63152
Petrik Diak <diak@petrik.hu>
2023-11-27 15:00:09 +0100

    Try it

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('index')
   @Render('index')
   index() {
     return { message: 'Welcome to the homepage' };
