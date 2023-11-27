54c3a34bc7bbd0aa5153e7efc53185fdf5d7c8ce
Petrik Diak <diak@petrik.hu>
2023-11-27 15:46:37 +0100

    Data Transfer Objects

---
+++ src/card.dto.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export class cardDto{
+  id: number;
+  name: string;
+  arena: number;
+  type: string;
+  elixir: number;
+  rarity: string;
+  description: string;
+  guessed: boolean;
+}
